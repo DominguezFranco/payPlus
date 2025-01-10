5c8c79d93b50cad7e1c63b3ab2f3148c4591bc57
Dominguez Franco <dominguezfranco939@gmail.com>
2025-01-09 23:59:38 -0300

    add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-
 import './App.css'
 import NavBar from './component/NavBar/NavBar'
 import Landing from './component/Landing/Landing'
+import Footer from './component/Footer/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +12,7 @@ function App() {
     <>
       <NavBar/>
       <Landing/>
+      <Footer/>
     </>
   )
 }
